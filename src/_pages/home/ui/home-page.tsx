bac000d159ad52940674aee48df99d446ea59937
alisher134 <alisherrakhmanov134@gmail.com>
2026-09-06 22:48:17 +0500

    chore: refactor project structure

--- src/_pages/home/ui/home-page.tsx
+++ src/_pages/home/ui/home-page.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function Home() {
+import { Button } from "@/shared/ui/button";
+
+export function HomePage() {
   return (
     <div>
       <Button>
