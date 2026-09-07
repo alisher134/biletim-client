0a21fe202456b6e1737bf85afd9f89bd4393ba51
alisher134 <alisherrakhmanov134@gmail.com>
2026-09-07 19:53:31 +0500

    feat: add @tanstack/react-query for data fetching

--- src/_app/providers/index.tsx
+++ src/_app/providers/index.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from "react";
 
+import { QueryProvider } from "./query-provider";
+
 export function AppProviders({ children }: PropsWithChildren) {
-  return children;
+  return <QueryProvider>{children}</QueryProvider>;
 }
